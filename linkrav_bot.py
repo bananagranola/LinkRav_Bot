770323b5218ff6751df9dfa0df62dfd597b0cd6e
bananagranola <rainermoerlinghartheim@yahoo.com>
2015-05-28 10:57:14 -0700

    use get_unread mentions and messages

--- linkrav_bot.py
+++ linkrav_bot.py
@@ -31,27 +31,6 @@ def signal_handler(signal, frame):
 	sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-# check if comment has already been processed
-def is_processed (reddit, comment):
-	already_processed = False
-
-	if re.search(comment.author.name, reddit_username, re.IGNORECASE):
-		already_processed = True
-		logger.debug("OWN POST: %s", comment.id)
-		return already_processed
-
-	# hacky workaround to populate child replies
-	comment = reddit.get_submission(comment.permalink).comments[0]
-
-	for comment_reply in comment.replies:
-		# check if direct child 
-		if comment_reply.parent_id == "t1_{}".format(comment.id):
-			# check if bot already commented
-			if re.search(comment_reply.author.name, reddit_username, re.IGNORECASE):
-				already_processed = True
-				logger.debug("ALREADY PROCESSED: %s", comment.id)
-				return already_processed
-
 # delete comments with lots of downvotes
 def delete_downvotes (user):
 	user_comments = user.get_comments(limit = 20)
@@ -62,17 +41,9 @@ def delete_downvotes (user):
 			logger.debug("DELETING: %s", user_comment.id)
 
 # process comments
-def process_comment (ravelry, reddit, comment):
+def process_comment (ravelry, comment):
 	comment_reply = ""
-	# check if comment called linkrav
-	if re.search('.*LinkRav.*', comment.body, re.IGNORECASE):
-		matches = re.findall(RAV_MATCH, comment.body, re.IGNORECASE)
-	else:
-		return ""
-
-	# check if previously processed
-	if is_processed (reddit, comment) == True:
-		return ""
+	matches = re.findall(RAV_MATCH, comment.body, re.IGNORECASE)
 
 	# iterate through matches
 	if matches is not None:
@@ -87,7 +58,7 @@ def process_comment (ravelry, reddit, comment):
 				comment_reply += match_string
 				comment_reply += "*****\n"
 
-	# log and post comment
+	# generate comment
 	if comment_reply != "":
 		comment_reply = START_NOTE + comment_reply + END_NOTE
 		logger.debug("\n\n-----%s-----\n\n", comment_reply)
@@ -95,7 +66,7 @@ def process_comment (ravelry, reddit, comment):
 	# return comment text
 	return comment_reply
 
-def main(subreddit):
+def main():
 
 	try:
 		# log into ravelry
@@ -103,25 +74,17 @@ def main(subreddit):
 
 		# log in to reddit
 		reddit = praw.Reddit('linkrav by /u/bananagranola')
-		try: reddit.login(reddit_username, reddit_password)
-		except praw.errors.InvalidUserPass, e:
-			logger.error("InvalidUserPass: %s", e.args)
-			sys.exit(1)
-
-		# retrieve subreddit
-		if subreddit is None:
-			subreddit = reddit.get_subreddit(bot_subreddit)
-		else: # for purposes of looping
-			subreddit.refresh()
+		reddit.login(reddit_username, reddit_password)
 
 		# retrieve comments
-		comments = subreddit.get_comments(limit = comments_limit)
+		comments = reddit.get_unread()
 		
 		# iterate through comments
 		for comment in comments:
 
 			# process comment
-			comment_reply = process_comment (ravelry, reddit, comment)
+			comment_reply = process_comment (ravelry, comment)
+			comment.mark_as_read()
 
 			# post, handling rate limit error
 			if comment_reply != "":
@@ -146,16 +109,15 @@ def main(subreddit):
 		logger.error('Timeout: %s', str(e.args))
 		sys.exit(1)
 	except praw.errors.ClientException, e:
-		logger.error('ClientException: %s', str(e.ERROR_TYPE))
+		logger.error('ClientException: %s', str(e.args))
 		sys.exit(1)
 	except praw.errors.ExceptionList, e:
-		logger.error('ExceptionList: %s', str(e.errors))
+		logger.error('ExceptionList: %s', str(e.args))
 		sys.exit(1)
 	except praw.errors.APIException, e:
-		logger.error('APIException: %s', str(e.ERROR_TYPE))
+		logger.error('APIException: %s', str(e.args))
 		sys.exit(1)
 
 if __name__ == "__main__":
-	subreddit = None
-	main (subreddit)
+	main()
 
