bfbe7e6a17bf555e7058e4e878f78c840395d4c1
Amy T. Cheng <amytcheng@ATCHENG-Linny>
2016-05-24 11:01:21 -0700

    save comments that don't generate replies

--- linkrav_bot.py
+++ linkrav_bot.py
@@ -98,12 +98,14 @@ def main():
 			# process comment and submit
 			comment_reply = process_comment (ravelry, comment)
                        
-                        reply = None
-                        if comment_reply != "":
-                            reply = comment.reply (comment_reply)
-                            logger.info(reply.permalink)
+			reply = None
+			if comment_reply != "":
+				reply = comment.reply (comment_reply)
+				logger.info(reply.permalink)
+			else:
+				comment.save()
 
-                        comment.mark_as_read()
+			comment.mark_as_read()
 
 		delete_downvotes(reddit.get_redditor(reddit_username))
 
